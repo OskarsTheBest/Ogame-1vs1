1ddc98495689b3a6fb263acb83a345f7db3cea03
Oskars <122702900+OskarsTheBest@users.noreply.github.com>
2023-02-13 18:58:43 +0200

    BIG CHANGES, now onto Wordle 1vs1 and try making that possible

---
+++ src/components/Cardturn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function cardTurn({chooseCard, val}) {
+  return (
+    <div className='turn' onclick={chooseCard}>
+      {val}
+    </div>
+  )
+}
+
+export default cardTurn
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,19 +1,25 @@
 import React, { useState } from 'react'
 import { Window, MessageList, MessageInput } from 'stream-chat-react'
 import './Chat.css';
+import MainGame from './Maingame';
 
 function Game({channel}) {
   const [playersJoined, setPlayersJoined] = useState(channel.state.watcher_count === 2);
 
 channel.on("user.watching.start", (event) => {
   setPlayersJoined(event.watcher_count === 2);
-});  
+});
+
+// winner
+const [result, setResult] = useState({winner: "none", state:"none"})
 if (!playersJoined){
         return <div>Waiting for other player to join</div>
     }
   return (
     <div className='gameContainer'>
-      <h1>Game</h1> 
+
+      <MainGame result ={result} setResult={setResult}/>
+
       <Window>
        <MessageList disableDateSeparator closeReactionSelectorOnClick messageActions={["react"]} hideDeletedMessages/>
        <MessageInput noFiles/>
